17b9234bc46a86c510609a80f85339d0901bfe45
jpra2 <jprandrade2@gmail.com>
2022-03-03 18:20:44 -0300

    add 404 html page

--- mfdw_site/settings.py
+++ mfdw_site/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8uq0-r=7(pcf52v^v@n3@cn1)!v45&l+b)s^e53v0jr^dlmwq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from .models import Page
 # Create your views here.
@@ -6,7 +6,7 @@ from .models import Page
 def index(request, pagename):
     # return HttpResponse("<h1>The Meandco Homepage</h1>")
     pagename = '/' + pagename
-    pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
     context = {
         'title': pg.title,
         'content': pg.body_text,
