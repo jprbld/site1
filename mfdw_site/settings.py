320dbb2584000e4944a365130b3f88ee75feb68e
jpra2 <jprandrade2@gmail.com>
2022-03-04 02:18:27 -0300

    add quotes

--- mfdw_site/settings.py
+++ mfdw_site/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'quotes.apps.QuotesConfig'
 ]
 
 MIDDLEWARE = [
